d93f1695f56dd1db445600290cb14608448f3d3a
djuodis <deividas73245@gmail.com>
2023-12-28 11:23:49 +0200

    init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Hero from './components/Hero'
+function App() {
+  return (
+    <>
+    <Hero title={'Hero'} subtitle={'Info hero'} color={'blue'} /> 
+    <Hero title={'About'} subtitle={'Info about'} color={'red'} /> 
+    <Hero title={'Contacts'} subtitle={'Info contacts'} color={'blue'} /> 
+    <Hero title={'Faq'} subtitle={'Info faq'} color={'red'} /> 
+    </>
+  )
+}
+
+export default App
